f3484b152305bb40bcfa0a90a299640230aebe35
Stanislav <stanislaworonin@yandex.ru>
2022-10-24 15:55:55 +0300

    add send email

--- src/routers/auth-router.ts
+++ src/routers/auth-router.ts
@@ -1,9 +1,8 @@
 import {Request, Response, Router} from "express";
 import {usersService} from "../domain/user-service";
 import {getAuthRouterMiddleware, postAuthRouterMiddleware} from "../middlewares/authRouter-middleware";
-import {authentication} from "../middlewares/validation-middleware/authentication";
 import {jwsService} from "../application/jws-service";
-
+import nodemailer from 'nodemailer'
 
 export const authRouter = Router({})
 
@@ -22,6 +21,37 @@ authRouter.post('/login',
         return res.status(200).send({accessToken: token})
     }
 )
+
+authRouter.post('/registration-email-resending', (req: Request, res: Response) => {
+
+})
+
+authRouter.post('/registration', async (req: Request, res: Response) => {
+    let transport = await nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'buckstabu030194@gmail.com',
+            pass: 'czxfvurrxhdrghmz',
+        }
+    })
+
+    let info = await transport.sendMail({
+        from: 'Stanislav <buckstabu030194@gmail.com>',
+        to: req.body.email,
+        subject: req.body.subject,
+        html: req.body.message
+    })
+
+    res.send({
+        "email": req.body.email,
+        "message": req.body.message,
+        "subject": req.body.subject
+    })
+})
+
+authRouter.post('/registration-confirmation', async (req: Request, res: Response) => {
+
+})
 
 authRouter.get('/me',
     getAuthRouterMiddleware,
