dc1bb19e47967e1c50f0a1e175dec996bc505ad5
Stanislav <stanislaworonin@yandex.ru>
2022-10-26 14:51:50 +0300

    fix email validation auth/registration

--- src/middlewares/authRouter-middleware.ts
+++ src/middlewares/authRouter-middleware.ts
@@ -1,13 +1,9 @@
 import {authentication} from "./validation-middleware/authentication";
-import {loginValidation,
-        passwordValidation} from "./validation-middleware/authRouter-validation";
 import {inputValidation} from "./validation-middleware/input-validation";
-import {userEmailValidation,
-        userLoginValidation,
-        userPasswordValidation} from "./validation-middleware/userRouter-validation";
+import {emailValidationForAuthRouter,
+        userBodyParemetersValidation} from "./validation-middleware/userRouter-validation";
 
 export const getAuthRouterMiddleware = [authentication]
-export const postAuthRouterMiddleware = [loginValidation, passwordValidation, inputValidation]
-export const postRegistrationMiddleware = [userLoginValidation, userPasswordValidation, userEmailValidation, inputValidation]
-
-export const postResendingRegistrationEmailMiddleware = [userEmailValidation, inputValidation]
+export const postAuthRouterMiddleware = [...userBodyParemetersValidation, inputValidation]
+export const postRegistrationMiddleware = [...userBodyParemetersValidation, inputValidation]
+export const postResendingRegistrationEmailMiddleware = [...emailValidationForAuthRouter, inputValidation]
--- src/middlewares/validation-middleware/authRouter-validation.ts
+++
@@ -1,4 +0,0 @@
-import {body} from "express-validator";
-
-export const loginValidation = body('login').isString().trim()
-export const passwordValidation = body('password').isString().trim()
