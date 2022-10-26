4f46c057751568633e8f203487db22a06387d6bf
Bucktabu <stanislaworonin@yandex.ru>
2022-10-26 18:16:13 +0300

    refactoring email validation

--- src/middlewares/validation-middleware/userRouter-validation.ts
+++ src/middlewares/validation-middleware/userRouter-validation.ts
@@ -2,4 +2,4 @@ import {body} from "express-validator";
 
 export const userLoginValidation = body('login').isString().trim().isLength({min: 3, max: 10})
 export const userPasswordValidation = body('password').isString().trim().isLength({min: 6, max: 20})
-export const userEmailValidation = body('email').isString().trim().isEmail()
+export const userEmailValidation = body('email').isString().trim().matches(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/)
