eaaf9f9f5931cc1fc662b570cf49da7b95c6626c
Stanislav <stanislaworonin@yandex.ru>
2022-10-26 15:10:14 +0300

    fix м1

--- src/middlewares/authRouter-middleware.ts
+++ src/middlewares/authRouter-middleware.ts
@@ -1,9 +1,10 @@
 import {authentication} from "./validation-middleware/authentication";
 import {inputValidation} from "./validation-middleware/input-validation";
-import {emailValidationForAuthRouter,
-        userBodyParemetersValidation} from "./validation-middleware/userRouter-validation";
+import {userEmailValidation,
+        userLoginValidation,
+        userPasswordValidation} from "./validation-middleware/userRouter-validation";
 
 export const getAuthRouterMiddleware = [authentication]
-export const postAuthRouterMiddleware = [...userBodyParemetersValidation, inputValidation]
-export const postRegistrationMiddleware = [...userBodyParemetersValidation, inputValidation]
-export const postResendingRegistrationEmailMiddleware = [...emailValidationForAuthRouter, inputValidation]
+export const postAuthRouterMiddleware = [userLoginValidation, userPasswordValidation, userEmailValidation, inputValidation]
+export const postRegistrationMiddleware = [userLoginValidation, userPasswordValidation, userEmailValidation, inputValidation]
+export const postResendingRegistrationEmailMiddleware = [userEmailValidation, inputValidation]
--- src/middlewares/validation-middleware/userRouter-validation.ts
+++ src/middlewares/validation-middleware/userRouter-validation.ts
@@ -2,7 +2,4 @@ import {body} from "express-validator";
 
 export const userLoginValidation = body('login').isString().trim().isLength({min: 3, max: 10})
 export const userPasswordValidation = body('password').isString().trim().isLength({min: 6, max: 20})
-export const userEmailValidation = body('email').isString().trim().notEmpty().isEmail()
-
-export const emailValidationForAuthRouter = [userEmailValidation]
-export const userBodyParemetersValidation = [userLoginValidation, userPasswordValidation, userEmailValidation]
+export const userEmailValidation = body('email').isString().trim().isEmail()
