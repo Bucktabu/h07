5b76a26517644b050ce6f6a750dca8940f56d736
Stanislav <stanislaworonin@yandex.ru>
2022-10-28 08:57:51 +0300

    fix email validation for registration-email-resending v9

--- src/middlewares/validation-middleware/resendingEmail-validation.ts
+++ src/middlewares/validation-middleware/resendingEmail-validation.ts
@@ -11,7 +11,7 @@ export const resendingEmailValidation = async (req: Request, res: Response, next
 
     const userEmailConfirmation = await emailConfirmationRepository.giveEmailConfirmationByCodeOrId(user.id)
 
-    if (userEmailConfirmation!.isConfirmed) {
+    if (userEmailConfirmation!.isConfirmed === true) {
         return res.status(400).send({errorsMessages: [{message: 'Your account is already verified', field: "email"}]})
     }
 
